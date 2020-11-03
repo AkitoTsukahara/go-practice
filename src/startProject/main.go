edaef5d66c81de606c793de06226eb24ff3f8c7e
AkitoTsukahara <tsukahara.akito@gmail.com>
2020-11-03 23:02:07 +0900

    [add]go lesson

--- src/startProject/main.go
+++ src/startProject/main.go
@@ -1,11 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//"time"
+	//"os/user"
+)
 
-func init() {
-	fmt.Println("Init!")
-}
+// func init() {
+// 	fmt.Println("Init!")
+// }
 
 func main() {
-	fmt.Println("Hello world","111","222")
+	var (
+		i   int     = 1
+		f64 float64 = 1.2
+		s   string  = "test"
+		t   bool    = true
+		f   bool    = false
+	)
+
+	fmt.Println(i, f64, s, t, f)
+
+	xi := 1
+	xf64 := 1.2
+	xs := "test"
+	xt, xf := true, false
+
+	fmt.Println(xi, xf64, xs, xt, xf)
+	fmt.Printf("%T", xf64)
 }
